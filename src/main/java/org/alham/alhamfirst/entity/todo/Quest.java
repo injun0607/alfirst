2f9944c69c7abe47659d237d0b4bc348d27da1b2
injun0607 <injun0607@mydongsim.com>
2024-10-02 18:03:23 +0900

    2024_1002

---
+++ src/main/java/org/alham/alhamfirst/entity/todo/Quest.java
@@ -0,0 +1,16 @@
+package org.alham.alhamfirst.entity.todo;
+
+import org.alham.alhamfirst.common.enums.Category;
+
+public class Quest {
+
+    private String title;
+
+    private Category category;;
+
+    private String info;
+
+
+
+
+}
